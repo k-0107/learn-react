8de4066e4359635748cf9312eb40a1ff3b2e251d
k-0107 <k.tkm0107@gmail.com>
2023-12-14 17:40:13 +0900

    APIレスポンスの表示成功

--- src/components/Test.js
+++ src/components/Test.js
@@ -4,10 +4,21 @@ function Test() {
   const [data, setData] = useState(null);
   const handleButtonClick = async () => {
     try {
-      const response = await fetch("http://localhost:8080/api/v1");
+      const response = await fetch("/api/v1");
 
-      const jsonData = await response.json();
-      console.log(jsonData);
+      // レスポンスが成功した場合、JSONデータを取得
+      if (response.ok) {
+        const jsonData = await response.json();
+        setData(jsonData);
+        console.log(jsonData);
+      } else {
+        // レスポンスがエラーの場合はエラーメッセージをコンソールに表示
+        console.error(
+          "Error fetching data:",
+          response.status,
+          response.statusText
+        );
+      }
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -16,6 +27,12 @@ function Test() {
     <div className="test">
       <h1>データ取得</h1>
       <button onClick={handleButtonClick}>クリック</button>
+      {data && (
+        <div>
+          <h2>API Response:</h2>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 }
