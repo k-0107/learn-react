452147d1d91e833ae633e6d4192b8e57b0b2fc61
k-0107 <k.tkm0107@gmail.com>
2023-12-20 16:21:04 +0900

    メッセージのe一覧表示

--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 
 function Sample() {
-  const [formData, setFormData] = useState({ message: "Test!!!" });
+  const [formData, setFormData] = useState({
+    message: "Test!!!",
+  });
+
+  const [submittedMessages, setSubmittedMessages] = useState([]);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault(); // ここでデフォルトのイベントをキャンセル
 
     try {
-      const response = await fetch("/messege ", {
+      const response = await fetch("/message ", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -18,7 +26,12 @@ function Sample() {
       if (response.ok) {
         console.log("データを送信しました");
         console.log(formData);
-        // 成功時の処理
+
+        setSubmittedMessages([...submittedMessages, formData.message]);
+
+        setFormData({
+          message: "",
+        });
       } else {
         console.error("データの送信に失敗しました");
         // エラー時の処理
@@ -30,9 +43,28 @@ function Sample() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <button type="submit">送信</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label id="message">
+          メッセージ:
+          <input
+            type="text"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">送信</button>
+      </form>
+      <div>
+        <h2>メッセージ一覧:</h2>
+        <ul>
+          {submittedMessages.map((message, index) => (
+            <li key={index}>{message}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
